c0e3463b5e456c46fa30ab6426fa2dab26f0f159
Jonathan Orellana <Jonathan.Orellana2@nscorp.com>
2023-05-02 17:01:02 -0400

    Create FibonacciNumbers.js

---
+++ FibonacciNumbers.js
@@ -0,0 +1,15 @@
+function fiboEvenSum(n) {
+  let numbers = [];
+  let startNumber = 0;
+  let nextNumber = 1;
+  for (let i = 0; i <= n; i++){
+    let newNumber = startNumber + nextNumber
+    if (newNumber % 2 === 0 && newNumber <= n){
+      numbers.push(newNumber);
+    }
+    nextNumber = startNumber
+    startNumber = newNumber
+  }
+  let summation = numbers.reduce((a,b)=>a+b);
+  return summation;
+}
